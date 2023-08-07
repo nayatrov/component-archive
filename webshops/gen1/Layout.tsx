339f4289a05b7505aaf79c3e4e1b6e50635609c1
天上天下唯我獨尊 <138163032+tenjotenge@users.noreply.github.com>
2023-08-07 01:24:48 -0400

    layout comments and fixing props

--- webshops/gen1/Layout.tsx
+++ webshops/gen1/Layout.tsx
@@ -11,7 +11,20 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+// navButtons are passed to the Navbar from here
+// and were chosen as these 3 because first webshop was 
+// relatively simple, but Navbar component can be
+// passed as many buttons and routes as you need
+
+// also made it so the text across the Banner component
+// is passed to it from here
+
+// Layout is incorporated into every page of my
+// personal webshop including checkout/cart, but some
+// sites choose to exclude banners and such from 
+// their checkout pages so it is up to you 
+
+const Layout: React.FC<LayoutProps> = () => {
   const navButtons: NavButton[] = [
     { text: 'Home', href: '/' },
     { text: 'Contact', href: '/contact' },
